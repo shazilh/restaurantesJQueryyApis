8e75f537bfa2277890fa1e000fce4f4e48e8fb3d
Shazil Tovar <shazil@gmail.com>
2017-06-06 11:49:01 -0500

    terminadas version 3 y 4

--- js/main.js
+++ js/main.js
@@ -9,46 +9,6 @@ function initMap() {
     map: map
   });
 }
-//traer valores de clases de mapas
-$(".mapa").click(function(){
-    var latitud=$(this).data("lat");
-    var longitud=$(this).data("lng");
-    
-    var mapa = $("#map");
-
-})
-
-
-
-/*enlazar evento click a cada mapa
-    $mapaQuintonil.click(initMap2);
-    $mapaPujol.click(initMap3);
-
-function initMap2() {
-    var quintonil={lat:19.4308557, lng:-99.1938909};
-    var map=new
-    google.maps.Map(document.getElementById('map'),{
-        zoom:17,
-        center:quintonil
-    });
-    var marker = new google.maps.Marker({
-        position: quintonil,
-        map:map
-    }); 
-}
-function initMap3() {
-    var pujol={lat:19.432394, lng:-99.1969973};
-    var map=new
-    google.maps.Map(document.getElementById('map'),{
-        zoom:17,
-        center:pujol
-    });
-    var marker = new google.maps.Marker({
-        position: pujol,
-        map:map
-    }); 
-}*/
-
 
 var restaurantes = [
 	{
@@ -56,36 +16,66 @@ var restaurantes = [
 		"numero": "+51986161136",
 		"foto": "http://via.placeholder.com/200x200",
         "direccion": "Newton 55, Col.Polanco,Cd. de México",
+        "coordenadas":{
+            lat:19.4308557,
+            lng:99.1938909
+            
+        }
 	},
 	{
 		"nombre": "Pujol",
 		"numero": "+51986161136",
 		"foto": "http://via.placeholder.com/200x200",
-        "direccion": "Francisco Petrarca 254, Col. Polanco."
+        "direccion": "Francisco Petrarca 254, Col. Polanco.",
+        "coordenadas":{
+            lat:19.432394,
+            lng:-99.1969973
+            
+        }
 	},
 	{
 		"nombre": "Biko",
 		"numero": "+51986161136",
 		"foto": "http://via.placeholder.com/200x200",
-        "direccion": "Mazarik 407, Col.Polanco."
+        "direccion": "Mazarik 407, Col.Polanco.",
+        "coordenadas":{
+            lat:19.432113,
+            lng:-99.2009208
+            
+        }
 	},
 	{
 		"nombre": "Pangea",
 		"numero": "+51986161136",
 		"foto": "http://via.placeholder.com/200x200",
-        "direccion": "Bosques del Valle 110, Col.Bosques del Valle."
+        "direccion": "Bosques del Valle 110, Col.Bosques del Valle.",
+        "coordenadas":{
+                lat:25.6501038,
+                lng:-100.3785448
+
+        }
 	},
 	{
 		"nombre": "Amaranta",
 		"numero": "+51986161136",
 		"foto": "http://via.placeholder.com/200x200",
-        "direccion": "Francisco Murguía 402, Universidad, Toluca de Lerdo."
+        "direccion": "Francisco Murguía 402, Universidad, Toluca de Lerdo.",
+        "coordenadas":{
+                lat:19.2810466,
+                lng:-99.6586632
+
+            }
 	},
 	{
 		"nombre": "Sud 777",
 		"numero": "+51986161136",
 		"foto": "http://via.placeholder.com/200x200",
-        "direccion": "Blvd. de la Luz 777, Jardines del Pedregal."
+        "direccion": "Blvd. de la Luz 777, Jardines del Pedregal.",
+        "coordenadas":{
+                lat:19.3087534,
+                lng:-99.2115435
+
+            }
 	}
 ];
 
@@ -104,7 +94,7 @@ var plantillaRestaurantes = '<article class="row restaurants">' +
                 'Direccion: __direccion__' +
               '</span>' +
               '<div class="card-action">'+
-                 '<a href="#">'+
+                 '<a data-latitud="__latitud__" data-longitud="__longitud__" class="mapaRestaurante" href="#">'+
                 'Ver mapa:'+'</a>'+
               '</div>'+
             '</div>' +
@@ -114,6 +104,8 @@ var plantillaRestaurantes = '<article class="row restaurants">' +
 
 var cargarPagina = function () {
 	$("#search-form").submit(filtrarRestaurantes);
+    mostrarRestaurantes(restaurantes);
+    
 };
 
 var filtrarRestaurantes = function (e) {
@@ -132,11 +124,55 @@ var mostrarRestaurantes = function (restaurantes) {
 			.replace("__numero__", restaurante.numero)
 			.replace("__foto__", restaurante.foto)
             .replace("__direccion__", restaurante.direccion)
-            
-        
+            .replace("__latitud__",restaurante.coordenadas.lat)
+            .replace("__longitud__",restaurante.coordenadas.lng);
 	});
 	$(".restaurantes").html(plantillaFinal);
+    $(".mapaRestaurante").click(cambiarUbicacion);
+
 };
-var $mapa=
 
 $(document).ready(cargarPagina);
+//funciones para mandar llamar mapas
+function obtenerUbicacionActual() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(mostrarPosicionActual);
+  } else {
+    alert("Geolocalización no es soportado en tu navegador");
+  }
+}
+
+function mostrarPosicionActual(posicion) {
+  var latitud = posicion.coords.latitude;
+  var longitud = posicion.coords.longitude;
+
+  var coordenadas = {
+    lat: latitud,
+    lng: longitud
+  };
+
+  mostrarMapa(coordenadas);
+}
+
+function cambiarUbicacion() {
+  var latitud = $(this).data("latitud");
+  var longitud = $(this).data("longitud");
+
+  var coordenadas = {
+    lat: latitud,
+    lng: longitud
+  };
+
+  console.log(coordenadas);
+  mostrarMapa(coordenadas);
+}
+function mostrarMapa(coordenadas) {
+  var map = new google.maps.Map($('#map')[0], {
+    zoom: 17,
+    center: coordenadas
+  });
+  var marker = new google.maps.Marker({
+    position: coordenadas,
+    map: map
+  });
+}
